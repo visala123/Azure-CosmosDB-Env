3e587508acda06d19e74189ed2f296a0630bc412
visala123 <visala.k@algorims.com>
2025-07-09 20:15:27 +0800

    changed deploy.yml file infracost version

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstateaccount"
+    storage_account_name = "tfstateaccountv"
     container_name       = "tfstate"
     key                  = "cosmosdb-prod.tfstate"
   }
