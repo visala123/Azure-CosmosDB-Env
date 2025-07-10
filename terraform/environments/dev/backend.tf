6f92ec5e05126fa44b660b47b7ad1232129ac7e2
visala123 <visala.k@algorims.com>
2025-07-10 10:46:22 +0800

    changed deploy.yml file infracost version

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -5,12 +5,5 @@ terraform {
     container_name       = "tfstate"
     key                  = "cosmosdb-dev.tfstate"
   }
-   required_version = ">= 1.0"
-   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
   }
-}
 
--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -5,11 +5,5 @@ terraform {
     container_name       = "tfstate"
     key                  = "cosmosdb-prod.tfstate"
   }
-   required_version = ">= 1.0"
-   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
+  
 }
